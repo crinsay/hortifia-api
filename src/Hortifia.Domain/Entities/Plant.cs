b51fdce78054f94f269997ccddb21cec86a7c7bb
Crinsay <martyna.loboda@outlook.com>
2025-10-30 20:21:33 +0100

    Fix: Added plant common name property and missing entity configurations

--- src/Hortifia.Domain/Entities/Plant.cs
+++ src/Hortifia.Domain/Entities/Plant.cs
@@ -11,6 +11,7 @@ public class Plant
 {
     public string Id { get; set; } = default!;
     public string Name { get; set; } = default!;
+    public string CommonName { get; set; } = default!;
     public string? ImgBlobName { get; set; }
     public bool IsNearHeater { get; set; }
     public LightCondition LightCondition { get; set; } = LightCondition.Medium;
--- src/Hortifia.Infrastructure/Persistence/EntityConfigurations/PostLikesConfiguration.cs
+++ src/Hortifia.Infrastructure/Persistence/EntityConfigurations/PostLikesConfiguration.cs
@@ -8,7 +8,7 @@ internal class PostLikes : IEntityTypeConfiguration<PostLike>
 {
     public void Configure(EntityTypeBuilder<PostLike> builder)
     {
-        builder.HasKey(pk => new { pk.UserId, pk.PostId  });
+        builder.HasKey(pk => new { pk.UserId, pk.PostId });
 
         builder.HasOne(pl => pl.Post)
             .WithMany()
