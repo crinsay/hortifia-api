8059367d8d0de3b6900f18777dfbdb7bbdbcc823
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-11-02 19:08:36 +0100

    Fix: Added missing watering properties and removed redundant one in plant entity.

--- src/Hortifia.Infrastructure/Migrations/20251102165426_AddedLastWateringDateAndExpectedWateringDateAndRemovedWateringStatusInPlant.Designer.cs
+++ src/Hortifia.Infrastructure/Migrations/20251102165426_AddedLastWateringDateAndExpectedWateringDateAndRemovedWateringStatusInPlant.Designer.cs
@@ -4,6 +4,7 @@ using Hortifia.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hortifia.Infrastructure.Migrations
 {
     [DbContext(typeof(HortifiaDbContext))]
-    partial class HortifiaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251102165426_AddedLastWateringDateAndExpectedWateringDateAndRemovedWateringStatusInPlant")]
+    partial class AddedLastWateringDateAndExpectedWateringDateAndRemovedWateringStatusInPlant
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Hortifia.Infrastructure/Migrations/20251102165426_AddedLastWateringDateAndExpectedWateringDateAndRemovedWateringStatusInPlant.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hortifia.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedLastWateringDateAndExpectedWateringDateAndRemovedWateringStatusInPlant : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WateringStatus",
+                table: "Plants");
+
+            migrationBuilder.RenameColumn(
+                name: "WateringDate",
+                table: "Plants",
+                newName: "LastWateringDate");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ExpectedWateringDate",
+                table: "Plants",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ExpectedWateringDate",
+                table: "Plants");
+
+            migrationBuilder.RenameColumn(
+                name: "LastWateringDate",
+                table: "Plants",
+                newName: "WateringDate");
+
+            migrationBuilder.AddColumn<byte>(
+                name: "WateringStatus",
+                table: "Plants",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)0);
+        }
+    }
+}
