970ac3e471d22f142898b269e5c27b1e052a772a
Crinsay <martyna.loboda@outlook.com>
2025-11-05 21:53:10 +0100

    Feature: Added delete room endpoint

--- src/Hortifia.Application/Common/Interfaces/Repositories/IRoomsRepository.cs
+++ src/Hortifia.Application/Common/Interfaces/Repositories/IRoomsRepository.cs
@@ -10,5 +10,6 @@ public interface IRoomsRepository
     Task<Room?> GetByIdAsync(int roomId);
     Task<IEnumerable<RoomListDto>> GetAllDtosByUserIdAsync(string userId, string? searchPhrase);
     Task<int> CreateAsync(Room room);
+    Task DeleteAsync(Room room);
     Task SaveChangesAsync();
 }
---
+++ src/Hortifia.Application/Rooms/Commands/DeleteRoom/DeleteRoomCommand.cs
@@ -0,0 +1,9 @@
+﻿using Hortifia.Domain.Common;
+using MediatR;
+
+namespace Hortifia.Application.Rooms.Commands.DeleteRoom;
+
+public class DeleteRoomCommand : IRequest<Result>
+{
+    public int RoomId { get; init; }
+}
