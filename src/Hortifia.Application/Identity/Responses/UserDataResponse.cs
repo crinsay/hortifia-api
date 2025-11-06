c19c6e7ddadb7026e37226a3e20eba9e3c17d2be
Łukasz Karwacki <lkarwacki3@interia.pl>
2025-11-06 20:41:55 +0100

    Refactor: Adjusted code after review suggestions.

--- src/Hortifia.Application/Identity/Responses/UserDataResponse.cs
+++ src/Hortifia.Application/Identity/Responses/UserDataResponse.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Hortifia.Application.Identity.Responses;
+﻿namespace Hortifia.Application.Identity.Responses;
 
 public class UserDataResponse
 {
