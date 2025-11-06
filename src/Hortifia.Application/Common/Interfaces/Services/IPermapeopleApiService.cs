6fa45fcbe5bc8e12ca771b2adc077cd6a2ab855b
Crinsay <martyna.loboda@outlook.com>
2025-11-06 17:20:54 +0100

    Feature: Connected with permapeople api and added get byid and added get endpoints
    - Added PermapeopleApiService,
    - Configured http service,
    - Added getById and getPlants endpoints

---
+++ src/Hortifia.Application/Common/Interfaces/Services/IPermapeopleApiService.cs
@@ -0,0 +1,11 @@
+﻿using Hortifia.Application.Plants.Dtos;
+using Microsoft.Extensions.Configuration;
+
+namespace Hortifia.Application.Common.Interfaces.Services;
+
+public interface IPermapeopleApiService
+{
+    Task<IEnumerable<PlantDto>?> GetPlantsAsync();
+    Task<PlantDto?> GetPlantByIdAsync(int id);
+}
+
